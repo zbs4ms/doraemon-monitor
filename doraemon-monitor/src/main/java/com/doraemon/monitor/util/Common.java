01c8515f16313bf7536fe502fff6f5ffacd0c096
zbs <zbs4ms@163.com>
2017-07-31 00:07:45 +0800

    update

--- doraemon-monitor/src/main/java/com/doraemon/monitor/util/Common.java
+++ doraemon-monitor/src/main/java/com/doraemon/monitor/util/Common.java
@@ -7,4 +7,6 @@ public class Common {
 
     public final static String SMS_URL="http://121.40.152.170:9087/system-sms/api/send";
     public final static String SMS_TYPE="SystemWarning";
+    public final static String SMS_shopId="jskj";
+    public final static int SMS_NUMBER=1;
 }
